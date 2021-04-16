398379d6be74b5e91d534ba707de8c5254662994
Erik Schnetter <schnetter@gmail.com>
2021-04-15 22:29:10 -0400

    Remove unused code

--- examples/examples-cuda.jl
+++ examples/examples-cuda.jl
@@ -1,10 +1,8 @@
 module KernelFusionExamples
 
 using CUDA
-using ForwardDiff
 using InteractiveUtils
 using KernelFusion
-using StaticArrays
 
 ################################################################################
 
@@ -32,7 +30,8 @@ function run_simple_kernel(kernel)
     # Check the result
     @assert Array(B) == max.(300, 2 .* A) .+ 1
 
-    # show_kernel_code(kernel, B)
+    # @device_code_ptx run_kernel!(kernel, B)
+    # @device_code_sass run_kernel!(kernel, B)
 end
 
 ################################################################################
@@ -60,52 +59,8 @@ function run_parameterized_kernel(kernel)
 
     @assert Array(B) == max.(300, 2 .* A) .+ 1
 
-    # show_kernel_code(kernel, params, B)
-end
-
-################################################################################
-
-function make_kernel_derivative()
-    code = quote
-        function (params, x)
-            add1(x) = x + params.offset
-            times2′(x) = times2(params, x)
-            # logsumexp(x, y) = log2(exp2(x) + exp2(y)) # smooth max
-            # return (add1 ∘ (x -> logsumexp(300, x)) ∘ times2′)(x)
-            return (add1 ∘ (x -> max(300, x)) ∘ times2′)(x)
-        end
-    end
-    kernel = make_kernel(code)
-
-    return kernel
-end
-
-function run_kernel_derivative(kernel)
-    npoints = 3
-    A = Float32[i + 100j for i in 1:npoints, j in 1:npoints]
-    params = (factor=2, offset=1)
-
-    function cost(params′)
-        params = (factor=params′[1], offset=params′[2])
-        B1 = kernel(params, A[1])
-        B = typeof(B1).(copy(A))
-        run_kernel!(kernel, params, B)
-        return sum((B .- 400) .^ 2)
-    end
-    params′ = SVector{2,Float32}(params.factor, params.offset)
-    # @show cost(params′)
-
-    grad_cost = params′ -> ForwardDiff.gradient(cost, params′)
-    # @show grad_cost(params′)
-    # Calculate gradient via finite differences
-    function grad(f, x, δ)
-        return SVector{2,Float32}((f(x + [(j == i) * δ for j in 1:length(x)]) -
-                                   f(x - [(j == i) * δ for j in 1:length(x)])) /
-                                  2δ for i in 1:length(x))
-    end
-    δ = 0.001
-    # @show grad(cost, params′, δ)
-    @assert grad_cost(params′) ≈ grad(cost, params′, δ)
+    # @device_code_ptx run_kernel!(kernel, params, B)
+    # @device_code_sass run_kernel!(kernel, params, B)
 end
 
 end
